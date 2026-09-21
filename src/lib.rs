4a8691da414b0b85dea47a0409be206edc7285ec
OverrideDeveloper <OverrideDeveloper@protonmail.com>
2026-09-21 13:13:04 -0800

    Expose loaded record type

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod record;
 pub mod xml;
 
 pub use engine::{DatasetEngine, RecordSource};
-pub use record::{DatasetRecord, RecordDecoder, RecordError, RecordResult};
+pub use record::{DatasetRecord, LoadedRecord, RecordDecoder, RecordError, RecordResult};
 pub use xml::{XmlRecordStream, XmlStreamConfig};
 
 /// A source of bounded dataset records.
